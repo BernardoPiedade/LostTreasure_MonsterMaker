a260f56fa6d521a6739355704b6c3912b90be4b4
berni <berni@DESKTOP-BERNAS>
2024-03-15 20:39:15 +0000

    Adicionar arquivos de projeto.

---
+++ MonsterModel.cs
@@ -0,0 +1,25 @@
+﻿namespace LostTreasure_MonsterMaker
+{
+    class MonsterModel
+    {
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public int Level { get; set; }
+        public int Blood {  get; set; }
+        public int Aura {  get; set; }
+        public required string DefenseDie {  get; set; }
+        public required string SpellDefenseDie { get; set; }
+
+        public int Strength { get; set; }
+        public int Dexterity { get; set; }
+        public int Constitution { get; set; }
+        public int Intelligence { get; set; }
+        public int Social { get; set; }
+
+        public required string SkillMastery { get; set; }
+
+        public required string Weapons { get; set; }
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using OfficeOpenXml;
+using System.IO;
+using System.Collections;
+using System.Threading.Tasks;
+
+namespace LostTreasure_MonsterMaker
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            var file = new FileInfo("monsters.xlsx");
+
+            var monster = GetMonsterModels();
+
+            await SaveExcelFile(monster, file);
+        }
+
+        private static async Task SaveExcelFile(List<MonsterModel> monster, FileInfo file)
+        {
+            DeleteFileIfExists(file);
+
+            using (var package = new ExcelPackage(file))
+            {
+                var work_sheet = package.Workbook.Worksheets.Add("Monster(s)");
+
+                var range = work_sheet.Cells["A1"].LoadFromCollection(monster, true);
+                range.AutoFitColumns();
+
+                await package.SaveAsync();
+            }
+        }
+
+        private static void DeleteFileIfExists(FileInfo file)
+        {
+            if(file.Exists)
+            {
+                file.Delete();
+            }
+        }
+
+        private static List<MonsterModel> GetMonsterModels()
+        {
+            List<MonsterModel> output = new()
+            {
+                new() {Name = "bandid", Description = "desc", Level = 1, Blood = 10, Aura = 0, DefenseDie = "1d6", SpellDefenseDie = "1d4", Strength = 1, Dexterity = 2, Constitution = 3, Intelligence = 4, Social = 5, SkillMastery = "jump, stealth, steal", Weapons = "dagger, dagger"}
+            };
+
+            return output;
+        }
+    }
+}
